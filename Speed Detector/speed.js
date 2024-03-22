5b19ad8c9bf2830867a9053ed53c03c35b8a6acd
jeangathoni8 <jeangathoni39@gmail.com>
2024-03-22 13:08:45 +0300

    speed problem

---
+++ Speed Detector/speed.js
@@ -0,0 +1,6 @@
+//Listen for the button click event.
+//Get the value from the input field.
+//Calculate the demerit points if speed is above 70 km/s. (For every 5 km/s over 70, add one point.)
+//Display “Ok” if speed is below or equal to 70.
+//Display “Points: X” where X is the number of demerit points if speed is above 70 but points are 12 or less.
+//Display “License suspended” if the points are more than 12.
